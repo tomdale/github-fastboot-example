540d99f589caa3af741621a270d06c32c8b9bfb7
Tom Dale <tom@tomdale.net>
2016-01-25 16:04:15 -0500

    Use published ember-network

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,8 @@ import fetch from 'ember-network/fetch';
 export default Ember.Route.extend({
   model() {
     return fetch('https://api.github.com/users/tomdale/events')
-      .then(function(response) {
-        return response.json();
+      .then(res => {
+        return res.json();
       });
   }
 });
